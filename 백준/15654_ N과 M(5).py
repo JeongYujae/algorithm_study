cbfa8ee3d2cb17a1d9b27988de54a65ef2149947
Jeongyujae <morrisrt6851@naver.com>
2022-06-10 17:09:25 +0900

    15654

---
+++ 백준/15654_ N과 M(5).py
@@ -0,0 +1,18 @@
+n,m=map(int,input().split())
+
+li=sorted(list(map(int,input().split()))) #데이터 input 받고 오름차순으로 정렬
+
+data=[]
+
+def dfs():
+    if len(data)==m: 
+        print(' '.join(map(str,data))) 
+        return
+    
+    for i in range(len(li)): #범위는 주어진 리스트의 길이, n으로 써도 됨
+        if li[i] not in data:
+            data.append(li[i]) 
+            dfs()
+            data.pop()
+            
+dfs()
