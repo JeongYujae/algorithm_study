6eeba24f67471e4848d22ce4c9997f5239748bc1
Jeongyujae <morrisrt6851@naver.com>
2022-06-11 00:20:11 +0900

    15656

---
+++ 백준/15656_ N과 M(7).py
@@ -0,0 +1,17 @@
+n,m=map(int,input().split())
+
+li=sorted(list(map(int,input().split()))) #데이터 input 받고 오름차순으로 정렬
+
+data=[]
+
+def dfs():
+    if len(data)==m: #오름차순일때의 조건 포함
+        print(' '.join(map(str,data))) 
+        return
+    
+    for i in range(len(li)): #범위는 주어진 리스트의 길이, n으로 써도 됨
+        data.append(li[i]) 
+        dfs()
+        data.pop()
+            
+dfs()
